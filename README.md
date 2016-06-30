5e6d172fbe4292de7113e7170c7703f9169cab01
copleykj <copleykj@gmail.com>
2016-06-29 23:28:56 -0400

    package structure

---
+++ README.md
@@ -0,0 +1,7 @@
+# User Blocking #
+
+This package enables users to block other users. This is useful when one user wants to stop unwanted communication from another user.
+
+## Installation ##
+
+`meteor install socialize:user-blocking`
---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  name: 'socialize:user-blocking',
+  version: '1.0.0',
+  summary: 'Allow users to block each other',
+  git: 'https://github.com/copleykj/socialize-user-blocking.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.3');
+  api.use('ecmascript');
+  api.mainModule('commmon/common.js');
+});
