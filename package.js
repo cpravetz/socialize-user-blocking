e40d4e16023972a241c3257bc83a93ef37129e8b
Kelly Copley <copleykj@gmail.com>
2017-07-13 16:13:50 -0400

    depend on base-model explicitly

--- package.js
+++ package.js
@@ -1,16 +1,17 @@
 Package.describe({
-  name: 'socialize:user-blocking',
-  version: '1.0.0',
-  summary: 'Allow users to block each other',
-  git: 'https://github.com/copleykj/socialize-user-blocking.git',
+    name: 'socialize:user-blocking',
+    version: '1.0.0',
+    summary: 'Allow users to block each other',
+    git: 'https://github.com/copleykj/socialize-user-blocking.git',
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.3');
-  api.use([
-      'ecmascript',
-      'socialize:user-model@1.0.0'
-  ]);
-  api.mainModule('server/server.js', 'server');
-  api.mainModule('common/common.js');
+Package.onUse(function (api) {
+    api.versionsFrom('1.3');
+    api.use([
+        'ecmascript',
+        'socialize:base-model@1.0.0',
+        'socialize:user-model@1.0.0',
+    ]);
+    api.mainModule('server/server.js', 'server');
+    api.mainModule('common/common.js');
 });
