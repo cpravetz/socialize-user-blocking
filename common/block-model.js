369a9a461e3c2f6264ece0790379edd2a91852d8
Kelly Copley <copleykj@gmail.com>
2017-07-20 15:18:12 -0400

    linting autofix

--- common/block-model.js
+++ common/block-model.js
@@ -3,44 +3,44 @@ import SimpleSchema from 'simpl-schema';
 
 export const BlocksCollection = new Mongo.Collection('blocks');
 
-export class Block extends BaseModel{
-    constructor(document){
+export class Block extends BaseModel {
+    constructor(document) {
         super(document);
     }
     isDuplicate() {
-        return !!BlocksCollection.findOne({userId:this.userId, blockedUserId:this.blockedUserId});
+        return !!BlocksCollection.findOne({ userId: this.userId, blockedUserId: this.blockedUserId });
     }
 }
 
 Block.attachCollection(BlocksCollection);
 
-//Create the schema for a Block
+// Create the schema for a Block
 Block.appendSchema({
-    "userId":{
-        type:String,
-        regEx:SimpleSchema.RegEx.Id,
-        autoValue:function () {
-            if(this.isInsert){
+    userId: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id,
+        autoValue() {
+            if (this.isInsert) {
                 return this.userId;
             }
         },
         index: 1,
-        denyUpdate:true
+        denyUpdate: true,
     },
-    "blockedUserId":{
-        type:String,
-        regEx:SimpleSchema.RegEx.Id,
+    blockedUserId: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id,
         index: 1,
-        denyUpdate: true
+        denyUpdate: true,
     },
-    "date":{
-        type:Date,
-        autoValue:function() {
-            if(this.isInsert){
+    date: {
+        type: Date,
+        autoValue() {
+            if (this.isInsert) {
                 return new Date();
             }
         },
         index: -1,
-        denyUpdate:true
-    }
+        denyUpdate: true,
+    },
 });
