785d436717996826be521eb337ff89d8d377acc9
Kelly Copley <copleykj@gmail.com>
2017-11-16 12:53:09 -0500

    fix lintin issues

--- package.js
+++ package.js
@@ -1,3 +1,4 @@
+/* global Package*/
 Package.describe({
     name: 'socialize:user-blocking',
     version: '1.0.0',
@@ -5,7 +6,7 @@ Package.describe({
     git: 'https://github.com/copleykj/socialize-user-blocking.git',
 });
 
-Package.onUse(function (api) {
+Package.onUse(function _(api) {
     api.versionsFrom('1.3');
     api.use([
         'socialize:user-model@1.0.0',
