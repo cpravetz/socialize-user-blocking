811da0ed977badf7568eee724a84dc371bb307df
Kelly Copley <copleykj@gmail.com>
2021-06-28 00:01:18 -0400

    update deps and versionsFrom for compatibility

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 /* global Package*/
 Package.describe({
     name: 'socialize:user-blocking',
-    version: '1.0.5',
+    version: '1.0.6',
     summary: 'Allow users to block each other',
     git: 'https://github.com/copleykj/socialize-user-blocking.git',
 });
 
 Package.onUse(function _(api) {
-    api.versionsFrom('1.10.2');
+    api.versionsFrom(['1.10.2', '2.3']);
     api.use([
         'check',
         'reywood:publish-composite@1.7.3',
-        'socialize:user-model@1.0.4',
+        'socialize:user-model@1.0.5',
     ]);
     api.imply('socialize:user-model');
     api.mainModule('server/server.js', 'server');
